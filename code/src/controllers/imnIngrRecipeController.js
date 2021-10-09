a1392fbc9565de1ff44ece49c44de2edaee5afba
Lihess <she6569@gmail.com>
2021-10-10 02:57:25 +0900

    refactor : scheduler 따로 정의 #12

--- code/src/controllers/imnIngrRecipeController.js
+++ code/src/controllers/imnIngrRecipeController.js
@@ -148,19 +148,19 @@ module.exports = class ImnIngrRecipeController {
 }
 
 // 스케줄러, 매일 정각 사용기간이 3일 남은 식자재를 엔터티로 사용.
-const createIIRS = schedule.scheduleJob('0 0 0 1/1 * ? * ', async() => {
-    // 남은 사용기간이 3일인 식자재와 그 식자재를 사용하는 레시피를 검색하는 쿼리.
-    // rowNum을 이용하여 recipeOrnu를 일정하게 부여
-    const query = `
-        select @rowNum := @rowNum+1 as recipeOrnu, y.ref_num as refNum, y.ingr_ornu as ingrOrnu, x.recipe_num as recipeNum 
-        from recipe_ingr as x, ref_enroll_ingr as y, (SELECT @rowNum:=0) R
-        where (x.ingr_name = y.ingr_name) and datediff(y.expy_date, now()) = 3;
-    `
-    sequelize.query(query, { type : Sequelize.QueryTypes.SELECT }) // type : 중복 방식을 위해서
-            .then((list) => {
-                // IIR 대량 생성
-                IIR.bulkCreate(list)
-                    .then((result) => { console.log( result.length, "IIR Bluk Create Success ") })
-                    .catch((err) => { console.log("error : ", err) })
-            }).catch((err) => {  console.log("error : ", err) })
-})
+//const createIIRS = schedule.scheduleJob('0 0 0 1/1 * ? * ', async() => {
+//    // 남은 사용기간이 3일인 식자재와 그 식자재를 사용하는 레시피를 검색하는 쿼리.
+//    // rowNum을 이용하여 recipeOrnu를 일정하게 부여
+//    const query = `
+//        select @rowNum := @rowNum+1 as recipeOrnu, y.ref_num as refNum, y.ingr_ornu as ingrOrnu, x.recipe_num as recipeNum 
+//        from recipe_ingr as x, ref_enroll_ingr as y, (SELECT @rowNum:=0) R
+//        where (x.ingr_name = y.ingr_name) and datediff(y.expy_date, now()) = 3;
+//    `
+//    sequelize.query(query, { type : Sequelize.QueryTypes.SELECT }) // type : 중복 방식을 위해서
+//            .then((list) => {
+//                // IIR 대량 생성
+//                IIR.bulkCreate(list)
+//                    .then((result) => { console.log( result.length, "IIR Bluk Create Success ") })
+//                    .catch((err) => { console.log("error : ", err) })
+//            }).catch((err) => {  console.log("error : ", err) })
+//})
