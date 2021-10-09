089e010e387a69f96fc20b35b34ecd4fd730b673
Lihess <she6569@gmail.com>
2021-10-09 20:35:36 +0900

    chore :  dialect 수정정

--- code/src/models/index.js
+++ code/src/models/index.js
@@ -30,7 +30,7 @@ const sequelize = new Sequelize(
     process.env.DATABASE ,
     process.env.USERNAME, 
     process.env.PASSWORD,
-    dialect = "mysql",
+    { dialect : 'mysql' }
     //config
 )
 
