5b52aa0dff8f0fb35b2c244fde01573da6779ba3
Lihess <she6569@gmail.com>
2021-10-13 15:58:48 +0900

    fix : 날짜 기본값 변경

--- code/src/models/index.js
+++ code/src/models/index.js
@@ -27,10 +27,10 @@ const sequelize = new Sequelize(
   process.env.MYSQL_DATABASE,
   process.env.MYSQL_USERNAME, 
   process.env.MYSQL_PASSWORD,
-    { 
+    {
       host : process.env.MYSQL_HOSTNAME, 
       dialect : 'mysql', 
-      timezone : "+09:00" 
+      timezone : process.env.TZ
     }
 )
 
