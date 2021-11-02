fc91fd722f53c5eff4070c58ee8155de967c0b87
Lihess <she6569@gmail.com>
2021-11-02 23:32:27 +0900

    refactor : 스케줄러 쿼리 변경

--- code/src/utils/scheduler.js
+++ code/src/utils/scheduler.js
@@ -7,9 +7,12 @@ const createIIRS = () => {
     // 남은 사용기간이 3일인 식자재와 그 식자재를 사용하는 레시피를 검색하는 쿼리.
     // rowNum을 이용하여 recipeOrnu를 일정하게 부여
     const query = `
-        select @rowNum := @rowNum+1 as recipeOrnu, y.ref_num as refNum, y.ingr_ornu as ingrOrnu, x.recipe_num as recipeNum 
-        from recipe_ingr as x, ref_enroll_ingr as y, (SELECT @rowNum:=0) R
-        where (x.ingr_name = y.ingr_name) and datediff(y.expy_date, now()) = 3;
+        SELECT (CASE @GROUPING WHEN c.ingrOrnu+c.refNum THEN @RANK := @RANK + 1 ELSE @RANK := 1 END) AS recipeOrnu, (@GROUPING:=ingrOrnu+refNum),
+        refNum, ingrOrnu, recipeNum
+        FROM (select x.recipe_num as recipeNum, y.ingr_ornu as ingrOrnu,  y.ref_num as refNum
+	        from recipe_ingr as x, ref_enroll_ingr as y
+	        where (x.ingr_name = y.ingr_name) and datediff(y.expy_date, DATE_ADD(now(), INTERVAL 9 HOUR)) = 3
+	        order by y.ref_num, y.ingr_ornu) c, (SELECT @GROUPING := '', @RANK := 0) R;
     `
     sequelize.query(query, { type : Sequelize.QueryTypes.SELECT }) // type : 중복 방식을 위해서
             .then((list) => {
@@ -20,4 +23,6 @@ const createIIRS = () => {
             }).catch((err) => {  console.log("error : ", err) })
 }
 
-createIIRS();
+module.exports = createIIRS;
+
+//createIIRS();
